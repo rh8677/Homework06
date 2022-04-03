5ef9dc7c92acf9c904eb9ac47660fce8b5bdaae3
rh8677 <rh8677@g.rit.edu>
2022-04-03 13:47:39 -0400

    Worked on the classifier program

--- HW05_Classifier_Hu.py
+++ HW05_Classifier_Hu.py
@@ -13,25 +13,57 @@ if __name__ == '__main__':
             # The second cmd argument is the csv file we have to open and retrieve data from
             with open(sys.argv[1]) as csv_file:
                 read_data = csv.reader(csv_file)
+                all_records = []  # The normalized records that we will classify later
                 output_file = open("output.csv", "w", newline="")  # Puts the classified data into another csv file
                 write_data = csv.writer(output_file)
                 header = next(read_data)
                 header.append('Class')
                 write_data.writerow(header)  # Puts the header in the output file
+
+                # We first normalize values in each record
                 for row in read_data:
+                    cur_record = [0] * 7  # The record with normalized values
+
+                    # Append the normalized age into the record array
                     the_float = float(row[0].strip())
-                    rounded_num = round(the_float / 2) * 2
+                    norm_age = round(the_float / 2) * 2
+                    cur_record[0] = norm_age
 
-                    # If the normalized age is less than the number specified, it will belong in the Assam class
-                    if rounded_num <= 46:
-                        row.append('+1')
-                        write_data.writerow(row)
+                    # Append the normalized height into the record array
+                    the_float = float(row[1].strip())
+                    norm_height = round(the_float / 4) * 4
+                    cur_record[1] = norm_height
 
-                    # If the normalized age is greater than the number specified, it will belong in the Bhutan
-                    # class instead
-                    else:
-                        row.append('-1')
-                        write_data.writerow(row)
+                    # Append the normalized tail length into the record array
+                    the_float = float(row[2].strip())
+                    norm_tail = round(the_float / 2) * 2
+                    cur_record[2] = norm_tail
+
+                    # Append the normalized hair length into the record array
+                    the_float = float(row[3].strip())
+                    norm_hair = round(the_float / 2) * 2
+                    cur_record[3] = norm_hair
+
+                    # Append the normalized bang length into the record array
+                    the_float = float(row[4].strip())
+                    norm_bang = round(the_float / 2) * 2
+                    cur_record[4] = norm_bang
+
+                    # Append the normalized reach into the record array
+                    the_float = float(row[5].strip())
+                    norm_reach = round(the_float / 2) * 2
+                    cur_record[5] = norm_reach
+
+                    # Append the earlobe values into the record array
+                    earlobe = int(row[6].strip())
+                    cur_record[6] = earlobe
+
+                    all_records.append(cur_record)
+
+                # For each record with normalized values, we determine which class it belongs to
+                for record in all_records:
+                    print(record)
+
             csv_file.close()
             output_file.close()
 
